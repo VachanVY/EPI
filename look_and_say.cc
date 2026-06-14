0882d8141b22cfd083e5b094575500be5ea41101
Vachan V Y <vachanvy05@gmail.com>
2026-06-14 12:07:56 +0530

    Implement Look and Say sequence functionality

--- look_and_say.cc
+++ look_and_say.cc
@@ -3,9 +3,28 @@
 #include "test_framework/generic_test.h"
 using std::string;
 
+static string nextNumber(const string& s) {
+  string ret;
+  for (int i = 0; i < s.size(); i++) {
+    long long numOcc = 1;
+    char c = s[i];
+    // count num occurances
+    while (i + 1 < s.size() && s[i+1] == c) {
+      numOcc += 1;
+      i++;
+    }
+    ret += std::to_string(numOcc);
+    ret.push_back(c);
+  }
+  return ret;
+}
+
 string LookAndSay(int n) {
-  // TODO - you fill in here.
-  return "";
+  string ret = "1";
+  for (int i = 1; i < n; i++) {
+    ret = nextNumber(ret);
+  }
+  return ret;
 }
 
 int main(int argc, char* argv[]) {
