972fb48af6092c9d5f7c1039fa45026de205051a
Vachan V Y <vachanvy05@gmail.com>
2026-06-19 09:15:26 +0530

    bipartite graph

--- is_circuit_wirable.cc
+++ is_circuit_wirable.cc
@@ -1,18 +1,49 @@
+#include <cstddef>
 #include <stdexcept>
 #include <vector>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/timed_executor.h"
-using std::vector;
+using namespace std;
 
 struct GraphVertex {
+  /*
+  -1 => unvisited
+  1 => visited and colored 1
+  0 => visited and colored 0
+  */
   int d = -1;
   vector<GraphVertex*> edges;
 };
 
-bool IsAnyPlacementFeasible(vector<GraphVertex>* graph) {
-  // TODO - you fill in here.
+// return false for odd length cycle else true
+bool dfs(
+  GraphVertex& curr,
+  int curr_color,
+  vector<GraphVertex>& graph
+) {
+  curr.d = curr_color;
+
+  for (auto nei : curr.edges) {
+    if (nei->d != -1) { // visited
+      if (nei->d == curr_color) { // color doesn't match
+        return false;
+      }
+    } else { // not visited
+      if (!dfs(*nei, !curr_color, graph)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool IsAnyPlacementFeasible(vector<GraphVertex>& graph) {
+  for (auto& vertex : graph) {
+    if (vertex.d == -1 && !dfs(vertex, 0, graph))
+      return false;
+  }
   return true;
 }
 struct Edge {
@@ -44,7 +75,7 @@ bool IsAnyPlacementFeasibleWrapper(TimedExecutor& executor, int k,
     graph[e.from].edges.push_back(&graph[e.to]);
   }
 
-  return executor.Run([&] { return IsAnyPlacementFeasible(&graph); });
+  return executor.Run([&] { return IsAnyPlacementFeasible(graph); });
 }
 
 int main(int argc, char* argv[]) {
